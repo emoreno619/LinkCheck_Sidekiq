7677a1c913e666a9f354c35b607f85be71494865
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-22 22:53:29 -0700

    displays sites on index and fixed create route; needs nokogiri scrape and typhoeus requests

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,10 +1,16 @@
 class SitesController < ApplicationController
 	def index
+		@sites = Site.all
 		@site = Site.new
 	end
 
 	def create
-		
+		@site = Site.new params.require(:site).permit(:url)
+		if @site.save
+			redirect_to sites_path
+		else
+			render :index
+		end
 	end
 
 end
